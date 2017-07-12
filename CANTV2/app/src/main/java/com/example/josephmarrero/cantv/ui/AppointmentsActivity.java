fc701864d246f6da1f7d4fff5dac5a3f5f18868d
Joseph Marrero <marrero250@gmail.com>
2017-07-11 20:25:19 -0400

    Problemas con la transicion de fragment

--- CANTV2/app/src/main/java/com/example/josephmarrero/cantv/ui/AppointmentsActivity.java
+++ CANTV2/app/src/main/java/com/example/josephmarrero/cantv/ui/AppointmentsActivity.java
@@ -2,9 +2,12 @@ package com.example.josephmarrero.cantv.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -13,15 +16,18 @@ import android.view.MenuItem;
 
 import com.example.josephmarrero.cantv.R;
 import com.example.josephmarrero.cantv.data.prefs.SessionPrefs;
+import com.example.josephmarrero.cantv.fragments.ItemOneFragment;
+import com.example.josephmarrero.cantv.fragments.ItemThreeFragment;
+import com.example.josephmarrero.cantv.fragments.ItemTwoFragment;
 
 public class AppointmentsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appointments);
         BottomNavigationView bottomNavigationView = (BottomNavigationView)
                 findViewById(R.id.navigation);
-
 
         // Redirección al Login
         if (!SessionPrefs.get(this).isLoggedIn()) {
@@ -30,22 +36,54 @@ public class AppointmentsActivity extends AppCompatActivity {
             return;
         }
 
-
         setContentView(R.layout.activity_appointments);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        bottomNavigationView.setOnNavigationItemSelectedListener
+                (new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        Fragment selectedFragment = null;
+                        switch (item.getItemId()) {
+                            case R.id.action_item1:
+                                selectedFragment = ItemOneFragment.newInstance();
+                                break;
+                            case R.id.action_item2:
+                                selectedFragment = ItemTwoFragment.newInstance();
+                                break;
+                            case R.id.action_item3:
+                                selectedFragment = ItemThreeFragment.newInstance();
+                                break;
+                        }
+                        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.frame_layout, selectedFragment);
+                        transaction.commit();
+                        return true;
+                    }
+                });
+
+        //Manually displaying the first fragment - one time only
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_layout, ItemOneFragment.newInstance());
+        transaction.commit();
+
+        //Used to select an item programmatically
+        //bottomNavigationView.getMenu().getItem(2).setChecked(true);
+
+
+
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-
-
-
-
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
+
+
             }
         });
     }
